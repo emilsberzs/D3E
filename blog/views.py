bd6d36c52ba4243e823a6b91f84a91a26859561f
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2020-12-05 18:26:53 +0000

    Added comment functuality, had to flush db due to missing columns, couldnt figur out

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but dont save it to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form})
 
 
 class PostListView(ListView):
@@ -26,14 +55,6 @@ def post_list(request):
         # If requested page out of range, return last page
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts, 'page': page})
-
-
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, status='published',
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
 
 
 def post_share(request, post_id):
